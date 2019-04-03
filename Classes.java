22bea4161e2c6665d2fa6ab8af06f99388dd49da
Kevin <digitaldisarray@users.noreply.github.com>
2019-04-02 22:50:37 -0700

    Create Classes.java

---
+++ Classes.java
@@ -0,0 +1,17 @@
+
+public enum Classes {
+	/*
+	 * These are the player classes.
+	 * Each class has its own strengths and weaknesses.
+	 * 
+	 * GUNNER:
+	 * - Movement Speed: Average
+	 * - Shooting Speed: Average
+	 * - Reload Speed: Average
+	 * - Walls: Average Capacity, Average Life Span, Average Size
+	 * 
+	 */
+	// TODO: Make more classes & decide on their abilities and stats
+	// TODO: Make G rated names
+	GUNNER
+}
