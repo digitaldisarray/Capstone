222ba555854d069f89ca6129fbe54c9ca463a34b
digitaldisarray <electrichexagon8@gmail.com>
2019-05-10 12:31:24 -0700

    Test task added again

---
+++ src/gui/objects/tasks/TestingTask.java
@@ -0,0 +1,13 @@
+package gui.objects.tasks;
+
+import gui.objects.ButtonTask;
+
+public class TestingTask implements ButtonTask {
+
+	@Override
+	public void run() {
+		System.out.println("Zuccess");
+		
+	}
+	
+}
